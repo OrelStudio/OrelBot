293c83f8a16b422090f6e6f0d3ce85a9a8577849
Orel Shriki <30898660+OrelStudio@users.noreply.github.com>
2018-02-19 19:23:40 +0200

    multiply

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 'use strict'
 
-const path = require('path')
-
-const config = require('./config')
-
+const Discord = require('discord.js');
 const commando = require('discord.js-commando');
-const bot = new commando.Client();
+const client = new Discord.Client();
 
-bot.registry.registerGroup('list', 'List');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(path.resolve(__dirname, './commands'));
-
-bot.login(config.discord.token);
-console.log('I am up')
+module.exports = class MultiplyCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'multiply',
+      group: 'list',
+      memberName: 'multiply',
+      description: 'multiply command'
+    });
+    client.on('message' function (message) {
+      if (message.content === '!multiply') {
+      // if the message is !multiply.
+      let multiplytwo = (num, num2) => {
+        let getFirstNumber = String(message.content);
+        // the first number to multiply.
+        message.channel.send('please enter your first number');
+        // to enter the first number to multiply.
+        num = getFirstNumber
+        // replacing the getFirstNumber by num.
+        let getSecondNumber = String(message.content);
+        // the ssecond number to multiply.
+        message.channel.send('please enter your second number');
+        // to enter the second number to multiply.
+        num2 = getSecondNumber
+        // replacing the getSecondNumber by num2
+        let multiplied = num * num2
+        // multiplying num and num2
+      }
+      }
+    })
+  }
+}
